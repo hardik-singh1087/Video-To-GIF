ee69199b99096a9d1dc199a290140b2bb9f57bfe
hardik-singh1087 <rajawathardiksingh1087@gmail.com>
2025-03-05 17:12:44 +0530

    Atom file created

---
+++ src/store/atoms/videoFileState.jsx
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const videoFileState = atom({
+  key: "videoFileState",
+  defaulr: null,
+});
+
+export const gifFileState = atom({
+  key: "gifFileState",
+  defaulr: null,
+});
